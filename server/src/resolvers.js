26f9c6bb0f777a1b87fe72166adec5bca98ca5fa
ekater1na <ekaterina_l@tut.by>
2022-07-03 14:13:47 +0300

    feat: add fetch implementation

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,14 +1,25 @@
+import fetch from "node-fetch";
+
 const resolvers = {
   Query: {
     // returns an array of Tracks that will be used to populate the homepage grid of our web client
     tracksForHome: (_, __, { dataSources }) => {
       return dataSources.trackAPI.getTracksForHome();
     },
+    tracksForHomeFetch: async () => {
+      const baseUrl = "https://odyssey-lift-off-rest-api.herokuapp.com";
+      const res = await fetch(`${baseUrl}/tracks`);
+      return res.json();
+    },
   },
   Track: {
-    author: ({ authorId }, _, { dataSources }) => {
-      return dataSources.trackAPI.getAuthor(authorId);
+    author: async ({ authorId }, _, { dataSources }) => {
+      const baseUrl = "https://odyssey-lift-off-rest-api.herokuapp.com";
+      const res = await fetch(`${baseUrl}/author/${authorId}`);
+      return res.json();
+      // return dataSources.trackAPI.getAuthor(authorId);
     },
+    
   },
 };
 
